018b7255eb044c7fa9c2b9a2811b0cf3a8a26a91
Kien Nguyen Dang <kiennguyendang@kiens-mac.local>
2016-11-07 06:00:17 +0700

    Initial Commit

---
+++ Assignment_Day6_KienND9_Searchbar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Assignment_Day6_KienND9_Searchbar
+//
+//  Created by Kien Nguyen Dang on 11/7/16.
+//  Copyright © 2016 Kien Nguyen Dang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
